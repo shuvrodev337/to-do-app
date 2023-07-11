d9b18aff5938b836d69f0acdd36190215586eb47
unknown <shuvrodevmondal337@gmail.com>
2023-07-12 01:00:02 +0600

    added simple todo functionality

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 
 const TodoList = () => {
-    return (
+const [todos, setTodos] = useState([])
+    const handleAddTodo = (event)=>{
+
+        event.preventDefault()
+        const newTodo =  event.target.newTodo.value
+        console.log(newTodo);
+        const updatedTodos = [...todos, newTodo]
+        setTodos(updatedTodos)
+    }
+        return (
         <div>
-            This is todo list
-            <form >
-            <input type="text" name="" id="" />
-            <input type="button" value="" />
+            <div>
+            <form onSubmit={handleAddTodo} className="space-x-2">
+            <input type="text" placeholder="Type here" name="newTodo" className="input input-bordered input-accent w-full max-w-xs" />
+            <input type="submit" className="btn btn-primary" value="Add" />
             </form>
+            </div>
+            <div>
+            <h1 className="text-3xl mx-auto my-6">This is todo list</h1>
+                {
+                    todos.map((todo, i)=><li key={i}>{todo}</li>)
+                }
+            </div>
         </div>
     );
 };
