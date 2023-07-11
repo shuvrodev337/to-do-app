7eb7fe679e9a4a6b62d540e8fce37252ebcd5809
unknown <shuvrodevmondal337@gmail.com>
2023-07-12 00:40:43 +0600

    added add todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,6 +3,10 @@ const TodoList = () => {
     return (
         <div>
             This is todo list
+            <form >
+            <input type="text" name="" id="" />
+            <input type="button" value="" />
+            </form>
         </div>
     );
 };
